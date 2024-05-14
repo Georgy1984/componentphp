210a12fee28a99990ef4fa7f73e29c985647e5fc
JoJo26RUS <tariel1@yandex.ru>
2024-05-14 22:36:17 +0300

    add html-code for Remember functionality

--- includes/login.php
+++ includes/login.php
@@ -36,10 +36,10 @@ require_once 'init.php';
 
 ?>
 
-<form action="" method="post">
+<form action="" method="post" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
 
     <div class="">
-        <label for="email">email</label>
+        <label for="email">Email</label>
         <input type="text"  name="email" value="<?php echo Input::get('email')?>">
 
     </div>
@@ -50,8 +50,12 @@ require_once 'init.php';
     </div>
 
 
-    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>"> <br>
 
+    <label for="remember-me">
+        <input type="checkbox" id="remember-me" name="remember-me"> Запомнить меня </br> </br>
+
+    </label>
 
     <button type="submit" class="btn btn-default float-right">Войти</button>
 </form>
